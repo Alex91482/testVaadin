228f599e33d9814aa4a5d42cf1b952d03951525f
Alex91482 <74872120+Alex91482@users.noreply.github.com>
2022-02-07 23:23:52 +0300

    Delete derictory

--- demoVaadin3/src/main/java/org/example/util/jdbc/dao/MyAccountDAO.java
+++
@@ -1,9 +0,0 @@
-package org.example.util.jdbc.dao;
-
-import org.example.entity.MyAccount;
-
-public interface MyAccountDAO {
-    void saveMyAccount(String userName, String password);
-    MyAccount findMyAccount(String userName, String password);
-    void createTableMyAccount();
-}
